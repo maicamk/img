9cf395e493e20956783f131e8b273f6ad51ebaac
Dzung Do <dungdo@osd.vn>
2024-11-09 00:13:25 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,23 +3,22 @@ import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-toastify/ReactToastify.min.css';
 import 'react-photo-view/dist/react-photo-view.css';
-import { GoogleAnalytics } from '@next/third-parties/google'
+//import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "图床",
-  description: "图床",
+  title: "Free hosting image",
+  description: "Dịch vụ hosting hình ảnh miễn phí, dùng cho diễn đàn, bbcode, sử dụng nơi lưu trữ gốc là Telegram.",
 };
 
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="vi">
       <body className={inter.className}>{children}</body>
-      <GoogleAnalytics gaId="G-JVKEXR5XSG" />
     </html>
   );
 }
